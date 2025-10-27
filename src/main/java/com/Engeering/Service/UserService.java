5b3068265828f11ff27bcde73857467fe937f43d
Ashok <ashokakumbar06@gmail.com>
2025-10-27 08:46:50 +0530

    new

--- src/main/java/com/Engeering/Service/UserService.java
+++ src/main/java/com/Engeering/Service/UserService.java
@@ -3,11 +3,12 @@ package com.Engeering.Service;
 import com.Engeering.Entity.User;
 import com.Engeering.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@org.springframework.stereotype.Service
+@Service
 public class UserService {
 
     @Autowired
